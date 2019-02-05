beef6864643ce1817ef416cf311d6e8b057e2e8f
AlphaNiner1415 <manchesternd@gmail.com>
2019-02-05 22:03:58 +0700

    This is still not working

--- MyShoppingList.py
+++ MyShoppingList.py
@@ -1,29 +1,36 @@
+items2 ={}
+items2['ham'] =20
+items2['bacon'] = 40
+items2['beans'] = 20
+print(items2)
 class ShoppingList(object):
     def __init__(self, customerName, items, cart,bill):
         self.customerName = customerName
         self.items = items
         self.cart = cart
         self.bill = bill
-    def addToCart (self, items, cart):
-        user_choice = input("Enter which items you'd like to add to cart")
-        if user_choice in cart:
-            print("You already have that item in your cart!")
-        elif user_choice in items:
-            cart = cart.append(user_choice)
-            items.remove(user_choice)
-            print("That item has been added to your cart!")
-        else:
+    def addToCart (self, items, cart, userinp):
+        if user_choice not in items:
             print("Plese enter another item!! That item is not available!!")
+        else:
+            cart = cart.append(items[userinp])
+            items.pop(userinp)
+            print("That item has been added to your cart!")
+        print(cart)
     def checkOut (self, cart, items, bill):
         for i in cart:
-            if i == "Ham":
-                bill += items.get("Ham")
-            if i == "Bacon":
-                bill += items.get("Bacon")
+            if i == "ham":
+                bill += items.get('ham')
+            if i == "bacon":
+                bill += items.get('bacon')
             if i == "beans":
-                bill += items.get("beans")
+                bill += items.get('beans')
+        print("Here is your total bill: $" + bill)
+bobList = ShoppingList("Bob",items2,[],0)
+user_choice = input("Enter which items you'd like to add to your cart ")
+bobList.addToCart(items2,[],user_choice)
+print(bobList.cart)
 
 ## Items = Ham 20
 ## bacon 40
 ## Beans 30
-items = {"Ham": 20, "Bacon": 40, "beans":30}
