9e408b1a380111b01a3dde3539d0333268271f23
AlphaNiner1415 <manchesternd@gmail.com>
2019-02-06 22:10:49 +0700

    Oh my god like no progess

--- MyShoppingList.py
+++ MyShoppingList.py
@@ -8,14 +8,20 @@ class ShoppingList(object):
         self.customerName = customerName
         self.items = items
         self.cart = cart
+        cart = {}
         self.bill = bill
     def addToCart (self, items, cart, userinp):
+        
         if user_choice not in items:
             print("Plese enter another item!! That item is not available!!")
+            return cart, items
         else:
-            cart = cart.append(items[userinp])
+            print(items[userinp])
+            cart.update(items[userinp])
+            print(cart)
             items.pop(userinp)
             print("That item has been added to your cart!")
+            return cart, items
         print(cart)
     def checkOut (self, cart, items, bill):
         for i in cart:
@@ -25,11 +31,13 @@ class ShoppingList(object):
                 bill += items.get('bacon')
             if i == "beans":
                 bill += items.get('beans')
-        print("Here is your total bill: $" + bill)
-bobList = ShoppingList("Bob",items2,[],0)
+        print("Here is your total bill: $",bill)
+bobList = ShoppingList("Bob",items2,{},0)
+bobList.items = items2
 user_choice = input("Enter which items you'd like to add to your cart ")
-bobList.addToCart(items2,[],user_choice)
+bobList.addToCart(items2,bobList.cart,user_choice)
 print(bobList.cart)
+bobList.checkOut(bobList.cart,bobList.items, bobList.bill)
 
 ## Items = Ham 20
 ## bacon 40
