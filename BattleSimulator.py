017182d21c2522371b2dfac1ecb7a9a311d24931
AlphaNiner1415 <manchesternd@gmail.com>
2019-03-12 18:48:37 +0700

    Random implemented not all the way done tho

--- BattleSimulator.py
+++ BattleSimulator.py
@@ -31,16 +31,14 @@ class Player(object):
             return self.att - opponent.defe
     def isDamaged(self, damage):
         self.hp -= damage
-        return self.hp, damage
+        return self.hp
     def isDefeated(self):
         if self.hp == 0:
             self.gameover = 1
             return True
         else:
             return False
-    def userInput(self,inp):
-        inp = input("What do you want to do?")
-        return inp
+
 
 Monster = Player("The Monster",10,20,5,5,0,1,0)
 gameOver = 0
@@ -48,28 +46,41 @@ myPlayer = Player("name",10,20,5,10,0,1,0)
 def battleFunction(player1, player2,gameover,temp):
 
     print("Let the battle commence!")
-    while gameover == 0:
-        print("...")
-        if temp == "player":
-            print("It is your turn!")
-            sleep(1)
-            print("You attack!")
-            print("The monster have "+ str(player2.isDamaged(player1.attack(player2)))+" Hp left")
-            temp = "monster"
-        elif temp == "monster":
-            print("It is the monster's turn")
-            sleep(1)
-            print("The monster attacks!")
-            print("You have "+ str(player1.isDamaged(player2.attack(player1)))+" Hp left")
+    # while gameover == 0:
+    print("...")
+    if temp == "player":
+        print("It is your turn!")
+        sleep(1)
 
-            sleep(1)
-            temp = "player"
-        if (player1.isDefeated()):
-            print("Monster wins!")
-            gameover = 1
-        elif (player2.isDefeated()):
-            print("You win!")
-            gameover = 1
+        print(random.choice(["You attack!", "You lunge at the monster,weapon at the ready."]))
+        random.seed(random.randint(1,200))
+        num = random.randint(1,10)
+
+        if num <= player1.spd - player2.spd:
+            damage = player1.attack(player2)
+
+            print(random.choice(["You hit!", "Score! Your attack connects!", "Your weapon draws blood as it connects with the monster!"]))
+            print("The monster have "+ str(player2.isDamaged(damage))+" Hp left")
+        else:
+            print("You miss!")
+            print("The monster have "+ str(player2.hp) + " Hp left")
+        temp = "monster"
+
+    elif temp == "monster":
+        print("It is the monster's turn")
+        sleep(1)
+        print("The monster attacks!")
+        num2 = random
+        print("You have "+ str(player1.isDamaged(player2.attack(player1)))+" Hp left")
+
+        sleep(1)
+        temp = "player"
+    if (player1.isDefeated()):
+        print("Monster wins!")
+        gameover = 1
+    elif (player2.isDefeated()):
+        print("You win!")
+        gameover = 1
 if myPlayer.spd >= Monster.spd:
     battleFunction(myPlayer,Monster,gameOver, "player")
 elif myPlayer.spd < Monster.spd:
