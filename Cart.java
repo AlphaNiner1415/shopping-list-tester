493980e3e29f04347be0454316e42cd2f08e8d72
AlphaNiner1415 <manchesternd@gmail.com>
2019-03-13 17:11:56 +0700

    .java files not working have to let teacher see

--- Cart.java
+++ Cart.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
 public class Cart{
   public ArrayList shoppingList = new ArrayList();
   private boolean isIn = false;
 
   public Cart(){
-    shoppingList = "";
+    shoppingList.add("");
   }
-
-  public boolean checkIfIn(shoppingList, item, isIn){
-    return isIn;
+  public void addToCart(ArrayList item){
+    shoppingList.add(item);
+    System.out.println(shoppingList);
   }
+  // public boolean checkIfIn(ArrayList shoppingList, item, boolean isIn){
+  //   for(int i = 0; i< shoppingList.size();i++){
+  //     if(item.unique_id ==
+  //   }
+  //   return isIn;
+  // }
 }
---
+++ MyShoppingList.java
@@ -0,0 +1,7 @@
+public class MyShoppingList{
+  public static void main(String[] args) {
+    Cart cart1 = new Cart();
+    Item item1 = new Item("Apple",1, 40.0);
+    cart1.addToCart(item1);
+  }
+}
