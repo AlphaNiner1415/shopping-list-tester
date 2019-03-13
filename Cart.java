82531678baf5eb618ccfacb48c6374b518dbd30d
AlphaNiner1415 <manchesternd@gmail.com>
2019-03-13 13:18:15 +0700

    Cart in progress

---
+++ Cart.java
@@ -0,0 +1,12 @@
+public class Cart{
+  public ArrayList shoppingList = new ArrayList();
+  private boolean isIn = false;
+
+  public Cart(){
+    shoppingList = "";
+  }
+
+  public boolean checkIfIn(shoppingList, item, isIn){
+    return isIn;
+  }
+}
--- Item.java
+++ Item.java
@@ -1,11 +1,20 @@
 public class Item {
   public String name;
-  public Integer id;
+  public int unique_id;
   public Double price;
 
   public Item(String s1, Integer i1, Double d1) {
     name = s1;
-    id = i1;
+    unique_id = i1;
     price = d1;
+  }
+  public double getPrice(){
+    return price;
+  }
+  public String getName(){
+    return name;
+  }
+  public int getId(){
+    return unique_id;
   }
 }
