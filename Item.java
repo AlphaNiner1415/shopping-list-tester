8eedfa0b09a7d388cc75f2b953e6aebb3eb04cc5
AlphaNiner1415 <manchesternd@gmail.com>
2019-03-13 12:46:52 +0700

    Started process of transforming into java

---
+++ Item.java
@@ -0,0 +1,11 @@
+public class Item {
+  public String name;
+  public Integer id;
+  public Double price;
+
+  public Item(String s1, Integer i1, Double d1) {
+    name = s1;
+    id = i1;
+    price = d1;
+  }
+}
