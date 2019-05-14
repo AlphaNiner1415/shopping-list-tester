f4fd5b54d708f33d716c92086ffc7fa11f91a4f5
Anon <anon@Anons-Macbook-Pro.local>
2019-05-14 21:59:17 +0700

    Added function to take user input

--- MyShoppingList.java
+++ MyShoppingList.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class MyShoppingList{
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String userInput;
+    do {
+      System.out.print("Welcome to the shopping mall what do you want to do? ");
+      userInput = sc.next();
+    } while (userInput != "Exit");
     Cart myCart = new Cart();
     Item Banana = new Item("Banana",1,40.0);
+    Item Apple = new Item("Apple",2,30.0);
     myCart.addToCart(Banana);
     myCart.printCart();
     System.out.println(myCart.getTotal());
